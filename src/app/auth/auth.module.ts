b161f3e716e0648d98a2d6df86100e9436800ccc
julia tokmakova <julia_tokmakova00@mail.ru>
2020-07-09 13:31:46 +0300

    feat: add basic login page layout

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from '../auth/auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [LoginPageComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class AuthModule { }
