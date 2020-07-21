7cf7a713e772a3d57d4b1233c495c373ced38fa9
julia tokmakova <julia_tokmakova00@mail.ru>
2020-07-21 21:06:46 +0300

    feat: add modal window for delete comment

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,9 +6,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AnswerComponent } from './components/answer/answer.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+
 
 @NgModule({
-  declarations: [MainPageComponent, AnswerComponent, CommentComponent],
+  declarations: [
+    MainPageComponent,
+    AnswerComponent,
+    CommentComponent,
+    ModalDialogComponent,
+  ],
   imports: [
     CommonModule,
     ProfileRoutingModule,
