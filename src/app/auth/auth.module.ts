3ea52d35071b405fd6a51eaaad64f9430d998805
VladaIsAda <melkon031@gmail.com>
2020-07-25 02:07:42 +0300

    feat register-form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from '../auth/auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 @NgModule({
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, RegistrationPageComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
