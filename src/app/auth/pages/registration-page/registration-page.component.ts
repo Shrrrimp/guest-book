8404651a02d361a04c646b0f7e982969419066fe
julia tokmakova <julia_tokmakova00@mail.ru>
2020-07-30 13:01:12 +0300

    refactor: registration page

--- src/app/auth/pages/registration-page/registration-page.component.ts
+++ src/app/auth/pages/registration-page/registration-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-page',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPageComponent implements OnInit {
 
+  public form: FormGroup;
+  public imageName = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      login: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      passwordConfirm: new FormControl('', Validators.required)
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+    //   this.authService.logIn(this.login.value, this.password.value).subscribe((data) => {
+    //     this.router.navigate(['/home']);
+
+    //   }, (err) => {
+    //     this.isDataInvalid = true;
+    //     this.isEmailInvalid = false;
+    //     this.isPasswordInvalid = false;
+    //   });
+    // } else {
+    //   this.login.errors ? this.isEmailInvalid = true : this.isEmailInvalid = false;
+    //   this.password.errors ? this.isPasswordInvalid = true : this.isPasswordInvalid = false;
+    // }
+    }
+  }
+
+  onFileSelected($event) {
+    
   }
 
 }
